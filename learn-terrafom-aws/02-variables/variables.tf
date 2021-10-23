55508fff19afa66f21ea742f8dfa7704cd95f7c7
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-23 19:26:31 +0530

    Variables out sample

--- learn-terrafom-aws/02-variables/variables.tf
+++ learn-terrafom-aws/02-variables/variables.tf
@@ -1,3 +1,4 @@
+##String Data-type declaration
 variable "sample" {
     default = "Hello Sample"
 }
@@ -10,4 +11,24 @@ output "OUT_SAMPLE" {
 
 output "COMBINED_OUT_SAMPLE" {
     value = "${var.sample}, Welcome."
+}
+
+##Integer Data-type declaration , we can use Float data-type also
+variable "interger_sample" {
+   default = 1001.101
+}
+
+output "OUT_INTEGER" {
+    value = var.interger_sample
+}
+
+##Boolean Data-type : true or false
+
+variable "bool_sample" {
+    type = bool
+    default = true
+}
+
+output "OUT_BOOL" {
+    value = var.bool_sample
 }
