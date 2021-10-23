ef2f899d820e0200277220864e93c322eb8fb1dd
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-23 19:09:19 +0530

    Variables out sample

--- learn-terrafom-aws/02-variables/variables.tf
+++ learn-terrafom-aws/02-variables/variables.tf
@@ -5,3 +5,9 @@ variable "sample" {
 output "OUT_SAMPLE" {
     value = var.sample
 }
+
+//Combined output sample output print
+
+output "COMBINED_OUT_SAMPLE" {
+    value = "$(var.sample), Welcome."
+}
