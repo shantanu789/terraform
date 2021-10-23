c59c59d209c1163dfc5017f7dcdb15ddedc69eda
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-23 19:44:51 +0530

    Variables out sample

--- learn-terrafom-aws/02-variables/variables.tf
+++ learn-terrafom-aws/02-variables/variables.tf
@@ -32,3 +32,33 @@ variable "bool_sample" {
 output "OUT_BOOL" {
     value = var.bool_sample
 }
+
+## Map variable type
+
+variable "sample_map" {
+    type = map(string)
+    description = "Map variable sample"
+    default = {
+        string1 = "Shan",
+        number = 7890,
+        boolean = true
+    }
+}
+
+output "OUT_SAMPLE_MAP" {
+    value = var.sample_map["string1"]
+    value1 = var.sample_map["number"]
+    value2 = var.sample_map["boolean"]
+}
+
+##List variable type
+
+variable "sample_list" {
+    default = [ Hello, 2000, false, "World" ]
+}
+
+output "OUT_SAMPLE_LIST" {
+    value = var.sample_list[0]
+    value1 = var.sample_list[1]
+    value2 = var.sample_list[3]
+}
