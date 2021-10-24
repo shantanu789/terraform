84bf7c20277e23ce6fcc95a1ab137aa974fbb0bb
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-25 03:56:26 +0530

    ec2 instance creation with SG

--- learn-terrafom-aws/03-ec2-with-sg/ec2_instance_create.tf
+++ learn-terrafom-aws/03-ec2-with-sg/ec2_instance_create.tf
@@ -42,8 +42,8 @@ resource "aws_security_group" "allow_ssh" {
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      self             = true
+      ipv6_cidr_blocks = []
+      self             = false
       prefix_list_ids  = []
       security_groups  = []
     }
@@ -56,8 +56,8 @@ resource "aws_security_group" "allow_ssh" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      self             = true
+      ipv6_cidr_blocks = []
+      self             = false
       prefix_list_ids  = []
       security_groups  = []
     }
