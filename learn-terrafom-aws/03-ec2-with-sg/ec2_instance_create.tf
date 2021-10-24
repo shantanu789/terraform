1fbdc38fa90c855e95bac7ae5c3c2c2d538b1842
Shantanu Unde <er.shantanuu@gmail.com>
2021-10-25 03:21:43 +0530

    ec2 instance creation with SG

---
+++ learn-terrafom-aws/03-ec2-with-sg/ec2_instance_create.tf
@@ -0,0 +1,61 @@
+terraform {
+    backend "remote" {
+        organization = "Shan-DevOps-Terraform" 
+        workspaces {
+            name = "terraform-aws"
+            }
+    }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "TF-EC2" {
+  ami           = "ami-0e4e4b2f188e91845"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "TF-EC2"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
